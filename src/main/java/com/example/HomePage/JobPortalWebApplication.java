6b42bdf2a568aa37a95f770b0eb2ad6a7b7e8a5b
varunsivasai <pattelavarunsivasai@gmail.com>
2025-10-24 20:26:35 +0530

    rename class name

--- src/main/java/com/example/HomePage/JobPortalWebApplication.java
+++ src/main/java/com/example/HomePage/JobPortalWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomePageApplication {
+public class JobPortalWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomePageApplication.class, args);
+		SpringApplication.run(JobPortalWebApplication.class, args);
 	}
 
 }
